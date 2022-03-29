3d09fb7fd4a07a28722918044f8057db628d19da
LabutinAS <99245289+LabutinAS@users.noreply.github.com>
2022-03-29 15:35:28 +0300

    Add files via upload

---
+++ TP_LB1_queue/main.cpp
@@ -0,0 +1,246 @@
+#include <iostream>
+#include <locale>
+#include "qPARENT.h"
+#include "qPRVT.h"
+#include "qPRTCTD.h"
+#include "qPBLC.h"
+
+using namespace std;
+
+/*11 - Подсчет числа элементов, больших среднего гармонического значения*/
+
+template <class T>
+void chosen_class(T* q, int n_q) //шаблонная функция для дальнейшей работы с классом
+{
+	setlocale(LC_ALL, "Rus");
+	int c;
+	int flag = 1;
+	int value;					//введенное значение пользователем
+	int index = 0;				//номер очереди, с которой работаем
+	int cnt = 0;				//переменная для выбора очереди
+	int res = 0;				//переменная для выполнения пункта задания
+	int chosen_q;				//номер выбранной очереди
+	while (flag == 1)
+	{
+		cout << "1 - Добавление элемента очереди" << endl;
+		cout << "2 - Удаление элемента очереди" << endl;
+		cout << "3 - Вывод очереди на экран" << endl;
+		cout << "4 - Количество элементов больших, чем среднее гармоническое очереди" << endl;
+		cout << "5 - Копирование очереди" << endl;
+		cout << "6 - Слияние двух очередей" << endl;
+		cout << "7 - Выбор иной очереди" << endl;
+		cout << "8 - С какой очередью я сейчас работаю?" << endl;
+		cout << "0 - Вернуться к выбору класса-наследника" << endl;
+		cout << "-> ";
+		cin >> c;
+		switch (c)
+		{
+		case 1: //добавление элемента очереди
+			system("cls");
+			cout << "Введите значение: ";
+			cin >> value;
+			q[index].push(value);
+			cout << "Значение добавлено в очередь.\n " << endl;
+			break;
+
+		case 2: //удаление элемента очереди
+			if (q[index].isEmpty())
+			{
+				cout << "Очередь уже пуста.\n " << endl;
+				system("pause");
+				break;
+			}
+			else
+			{
+				system("cls");
+				value = q[index].pop();
+				cout << "Удалён элемент: " << value << endl;
+				cout << "\n";
+			}
+			break;
+
+		case 3: //вывод очереди на экран
+			if (q[index].isEmpty())
+			{
+				cout << "Очередь пуста.\n" << endl;
+				system("pause");
+				break;
+			}
+			else
+			{
+				system("cls");
+				q[index].print();
+				cout << "\n";
+			}
+			break;
+
+		case 4: //Количество элементов больших, чем среднее гармоническое очереди
+			if (q[index].isEmpty())
+			{
+				cout << "Очередь пуста.\n" << endl;
+				system("pause");
+				break;
+			}
+			else
+			{
+				system("cls");
+				res = q[index].variant11();
+				cout << "Количество элементов больших, чем среднее гармоническое очереди: " << res << endl;
+				cout << "\n";
+			}
+			break;
+
+		case 5: //создание копии очереди
+			if (q[index].isEmpty())
+			{
+				cout << "Очередь пуста.\n" << endl;
+				system("pause");
+				break;
+			}
+			else
+			{
+				cout << "В какую очередь скопировать \n" << endl;
+				cin >> cnt;
+				if ((cnt < 0) || (cnt > n_q))
+				{
+					cout << "Некорректное значение!\n" << endl;
+					system("pause");
+					break;
+				}				
+				if (1 == n_q || cnt == index)
+				{
+					cout << "Невозможно создать копию очереди, некуда вставить\n" << endl;
+					system("pause");
+					break;
+				}
+					system("cls");
+					q[cnt].copy(q[index]);
+					cout << "Очередь успешно скопирована в очередь: №" << cnt << "\n" << endl;
+			}
+			break;
+
+		case 6: //слияние двух очередей
+			if (n_q == 1)
+			{
+				cout << "Существует только одна очередь.\n" << endl;
+				system("pause");
+				break;
+			}
+			else
+			{
+				cout << "С какой очередью произвести слияние ?" << endl;
+				cin >> chosen_q;
+				if ((chosen_q < 0) || (chosen_q >= n_q) || (chosen_q == index))
+				{
+					cout << "Некорректное значение!\n" << endl;
+					system("pause");
+					break;
+				}
+				if (q[chosen_q].isEmpty())
+				{
+					cout << "Невозможно произвести слияние, так как вторая очередь пуста.\n" << endl;
+					system("pause");
+					break;
+				}
+				system("cls");
+				q[index].merge(q[chosen_q]);
+				cout << "Слияние прошло успешно.\n" << endl;
+			}
+			break;
+
+		case 7: //выбор иной очереди
+			system("cls");
+			cout << "Сейчас вы работаете с очередью №" << index << endl;
+			cout << "Введите номер очереди (от 0 до " << n_q - 1 << ") , на которую хотите переключиться: ";
+			cin >> chosen_q;
+			if ((chosen_q < 0) || (chosen_q >= n_q) || (chosen_q == index))
+			{
+				cout << "Некорректное значение!\n" << endl;
+				system("pause");
+			}
+			else
+			{
+				index = chosen_q;
+				system("cls");
+				cout << "Вы переключились на очередь №" << index << endl;
+				cout << "\n";
+			}
+			break;
+
+		case 8:
+			system("cls");
+			cout << "Вы сейчас работаете с очередью №" << index << endl;
+			cout << "\n";
+			break;
+
+		case 0:
+			cout << "\n";
+			flag = 0;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int num_q;		//переменная количества очередей
+	int c;
+	int flag = 1;
+	sonQueue_private* q1 = NULL;
+	sonQueue_protected* q2 = NULL;
+	sonQueue_public* q3 = NULL;
+	do
+	{
+		cout << "Введите колчество очередей: ";
+		cin >> num_q;
+		if (num_q < 1)
+			cout << "Неккоректный ввод!" << endl;
+	} while (num_q < 1);
+
+	system("cls");
+	cout << "Выберите, с каким классом-наследником Вы будете работать: " << endl;
+	while (flag == 1)
+	{
+		cout << "1 - Приватный" << endl;
+		cout << "2 - Защищенный" << endl;
+		cout << "3 - Публичный" << endl;
+		cout << "0 - Выход из программы" << endl;
+		cout << "-> ";
+		cin >> c;
+
+		switch (c)
+		{
+		case 1:
+			q1 = new sonQueue_private[num_q];
+			system("cls");
+			chosen_class(q1, num_q);
+			delete[] q1;
+			break;
+
+		case 2:
+			q2 = new sonQueue_protected[num_q];
+			system("cls");
+			chosen_class(q2, num_q);
+			delete[] q2;
+			break;
+
+		case 3:
+			q3 = new sonQueue_public[num_q];
+			system("cls");
+			chosen_class(q3, num_q);
+			delete[] q3;
+			break;
+
+		case 0:
+			flag = 0;
+			break;
+
+		default:
+			system("cls");
+			cout << "Некорректный ввод!" << endl;
+			break;
+		}
+	}
+	return 0;
+}
